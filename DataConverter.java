1e1dadf6d7538812ff15cc7bb38dc0a1b0857447
revalong <111657921+revalong@users.noreply.github.com>
2023-02-23 16:57:24 -0600

    Update DataConverter.java

--- DataConverter.java
+++ DataConverter.java
@@ -1,29 +1,70 @@
 package com.fmt;
 
 import java.io.FileWriter;
+
 import java.io.IOException;
 import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.io.File;
 
 import com.google.gson.Gson;
-
-
-
+import com.google.gson.GsonBuilder;
 
 public class DataConverter {
-	public void DataConverter(ArrayList<Items> items, ArrayList<Stores> stores,ArrayList<Persons> persons) {
-		Gson gson = new Gson();
-		
-		// Serialization
-		//Needs to iterage through array lists of objects 
-		//T
-		gson.toJson(ints);     // ==> [1,2,3,4,5]
-		gson.toJson(strings);  // ==> ["abc", "def", "ghi"]
-
-		// Deserialization
-		int[] ints2 = gson.fromJson("[1,2,3,4,5]", int[].class);
-		// ==> ints2 will be same as ints
+	private ArrayList<Items> it;
+	private ArrayList<Stores> st;
+	private ArrayList<Persons> per;
+	
+	
+	public DataConverter(ArrayList<Items> it, ArrayList<Stores> st, ArrayList<Persons> per) {
+		this.it=it;
+		this.per=per;
+		this.st=st;
 	}
+
+		public void convert() {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		String persons = gson.toJson(per);
+		String items = gson.toJson(it);
+		String stores = gson.toJson(st);
+		try
+		{
+		    String filename= "Persons.json";
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    fw.write(persons);//appends the string to the file
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+		try
+		{
+		    String filename= "Items.json";
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    fw.write(items);//appends the string to the file
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+		try
+		{
+		    String filename= "Stores.json";
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    fw.write(stores);//appends the string to the file
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+		System.out.println("complete");
+	
+	}
+		public static void main(String[] args) {
+			
+		}
+
 }
