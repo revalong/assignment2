420fa4d04ac80ce172e887c7d0b99a6aab940073
revalong <111657921+revalong@users.noreply.github.com>
2023-02-22 20:45:49 -0600

    Update DataConverter.java

--- DataConverter.java
+++ DataConverter.java
@@ -1,17 +1,29 @@
 package com.fmt;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
 
 
 
 public class DataConverter {
-	public DataConverter() {
+	public void DataConverter(ArrayList<Items> items, ArrayList<Stores> stores,ArrayList<Persons> persons) {
+		Gson gson = new Gson();
+		
+		// Serialization
+		//Needs to iterage through array lists of objects 
+		//T
+		gson.toJson(ints);     // ==> [1,2,3,4,5]
+		gson.toJson(strings);  // ==> ["abc", "def", "ghi"]
+
+		// Deserialization
+		int[] ints2 = gson.fromJson("[1,2,3,4,5]", int[].class);
+		// ==> ints2 will be same as ints
 	}
-
-	
-	
-	
-
 }
